3f96fd3d1f34fd0c8c4e739adab12e6eff9178c6
Hernan Schmidt <lagartoflojo@gmail.com>
2014-09-08 21:37:13 -0300

    Dashboard show actual teams, mutants and tasks

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class DashboardControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
+    assert_includes assigns(:teams), teams(:phalanx)
+    assert_includes assigns(:teams), teams(:mannite)
     assert_response :success
   end
 
