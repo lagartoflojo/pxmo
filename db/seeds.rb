d6bea88f92762235697af625128fb2820f2406d5
Hernan Schmidt <lagartoflojo@gmail.com>
2014-09-08 21:28:54 -0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+phalanx = Team.create name: 'Phalanx'
+mannite = Team.create name: 'Mannite'
+
+['Attack on Titan', 'Take out the trash', 'Destroy the enemy'].each do |desc|
+  phalanx.tasks.create description: desc
+end
+
+['Organize mutant party', 'Invite mutants'].each do |desc|
+  mannite.tasks.create description: desc
+end
+
+%w(Cyclops Angel Beast Phoenix).each do |name|
+  phalanx.mutants.create name: name
+end
+
+%w(Mimic Changeling Havok Petra).each do |name|
+  mannite.mutants.create name: name
+end
